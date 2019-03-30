bea58df2cee5a3c59aca05617cfa5133416c7e7e
ajay sharma <ajaysh73@gmail.com>
2019-03-30 15:48:35 -0400

    first commit

---
+++ src/components/user/User.vue
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <button class="btn btn-primary" @click="gotoHome">Go to Home</button>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    gotoHome() {
+      this.$router.push("/");
+    }
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app');
